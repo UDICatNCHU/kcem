16fc9d4ff6557d8f79dc6e8277aec37428ea870c
david <davidtnfsh@gmail.com>
2017-10-09 21:06:56 +0800

    refine loss.py

--- loss.py
+++ loss.py
@@ -5,7 +5,13 @@ model = models.KeyedVectors.load_word2vec_format('med400.model.bin', binary=True
 data = json.load(open('Ontology_from_google.json', 'r')).items()
 file = {}
 
-def criteria(mode, myans):
+def criteria(mode, myans, key):
+	def cosSimilarity(x):
+		try:
+			return (x[0], model.similarity(key, x[0]))
+		except Exception as e:
+			pass
+
 	if mode == 'w2v':
 		myans = [i for i in myans if i[0] != key]
 		myans = list(map(cosSimilarity, myans[:3]))
@@ -24,32 +30,29 @@ def criteria(mode, myans):
 		myans = sorted(result.items(), key=lambda x:-x[1])
 		return myans[0][0]
 
+def main(kcmNum, kemNum):
+	loss = 0
+	total = 0
 
-for kcmNum in range(2, 30):
-	for kemNum in range(1, 30):
-		loss = 0
-		total = 0
+	for key, ans in pyprind.prog_bar(data):
+		myans = requests.get('http://140.120.13.244:10000/kcem/?keyword={}&kcm={}&kem={}&lang=cht'.format(key, kcmNum, kemNum)).json()
 
-		for key, ans in pyprind.prog_bar(data):
-			myans = requests.get('http://140.120.13.244:10000/kcem/?keyword={}&kcm={}&kem={}&lang=cht'.format(key, kcmNum, kemNum)).json()
+		if myans:
+			print(myans)
+			myans = criteria(sys.argv[1], myans, key)
+			try:
+				total += 1
+				loss += ((1-float(model.similarity(myans, ans)))*10)**2
+			except Exception as e:
+				print(e)
+				continue
+	return loss, total
 
-			def cosSimilarity(x):
-				global key
-				try:
-					return (x[0], model.similarity(key, x[0]))
-				except Exception as e:
-					pass
-			if myans:
-				print(myans)
-				myans = criteria(sys.argv[1], myans)
-				try:
-					total += 1
-					loss += ((1-float(model.similarity(myans, ans)))*10)**2
-				except Exception as e:
-					print(e)
-					continue
+if __name__ == '__main__':
 
-		print("finish {} test, total loss is {}".format(total, loss / total))
-		file['{}-{}'.format(kcmNum, kemNum)] = loss / total
-
-json.dump(file, open('kcem.loss.json','w'))
+	for kcmNum in range(2, 3):
+		for kemNum in range(1, 3):
+			loss, total = main(kcmNum, kemNum)
+			print("finish {} test, total loss is {}".format(total, loss / total))
+			file['{}-{}'.format(kcmNum, kemNum)] = loss / total
+	json.dump(file, open(sys.argv[2],'w'))
