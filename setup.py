669f2e18996f031672f202b8fb2749a4d653f4e8
david <davidtnfsh@gmail.com>
2017-10-14 12:04:17 +0800

    remove tensorflow + keras from setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ setup(
     name = 'kcem',
     packages = ['kcem'],
     package_data={'kcem':['utils/*']},
-    version = '2.2',
+    version = '2.3',
     description = 'kcem class file',
     author = 'davidtnfsh',
     author_email = 'davidtnfsh@gmail.com',
     url = 'https://github.com/udicatnchu/kcem',
-    download_url = 'https://github.com/udicatnchu/kcem/archive/v2.2.tar.gz',
+    download_url = 'https://github.com/udicatnchu/kcem/archive/v2.3.tar.gz',
     keywords = ['kcem'],
     classifiers = [],
     license='GPL3.0',
@@ -17,11 +17,7 @@ setup(
         'pymongo',
         'simplejson',
         'requests',
-        'keras',
-        'click',
         'jieba',
-        'tensorflow',
-        'h5py'
     ],
     zip_safe=True
 )
