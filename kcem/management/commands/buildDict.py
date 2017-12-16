bb3e47363fcdf1505f237791d6cd8f8447f2d2dd
david <davidtnfsh@gmail.com>
2017-12-16 12:02:47 +0800

    [Bug fix] indentation error

--- kcem/management/commands/buildDict.py
+++ kcem/management/commands/buildDict.py
@@ -6,16 +6,16 @@ class Command(BaseCommand):
     help = 'use this to test kcem!'
 
     def handle(self, *args, **options):
-		client = pymongo.MongoClient(None)['nlp']
-		reverseCollect = client['wikiReverse']
-		Collect = client['wiki']
+        client = pymongo.MongoClient(None)['nlp']
+        reverseCollect = client['wikiReverse']
+        Collect = client['wiki']
 
-		dictionary = {i['key'] for i in reverseCollect.find()}
-		dictionary.update({i['key'] for i in Collect.find()})
-		final = list(filter(lambda x:'template:' not in x and 'talk:' not in x and 'user:' not in x and 'category talk:' not in x and 'wikipedia:' not in x and 'draft:' not in x and 'portal:' not in x and 'wikipedia:' not in x and 're:start' not in x and '模塊:' not in x and 'help:' not in x, dictionary))
+        dictionary = {i['key'] for i in reverseCollect.find()}
+        dictionary.update({i['key'] for i in Collect.find()})
+        final = list(filter(lambda x:'template:' not in x and 'talk:' not in x and 'user:' not in x and 'category talk:' not in x and 'wikipedia:' not in x and 'draft:' not in x and 'portal:' not in x and 'wikipedia:' not in x and 're:start' not in x and '模塊:' not in x and 'help:' not in x, dictionary))
 
-		with open('wiki.dict.txt', 'w') as f:
-		    for i in final:
-		        f.write('{} 999 n\n'.format(i))
+        with open('wiki.dict.txt', 'w') as f:
+            for i in final:
+                f.write('{} 999 n\n'.format(i))
 
         self.stdout.write(self.style.SUCCESS('dump wiki dictionary success!!!'))
